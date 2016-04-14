ecbd2ff7a2e58dfc0a985e6764bcd92f55b8a802
anatolij94 <anatolij94@yandex.ru>
2016-04-14 22:58:08 +0300

    add step_GOD//CODUNOV

---
+++ step.py
@@ -0,0 +1,8 @@
+from useful_features import *
+def step_GOD(u_prev,t):
+    u_curr=[]
+    u_curr.append(uL(t))
+    for i in range(1,len(u_prev)):
+        u_curr.append((1-q)*u_prev[i]+q*u_prev[i-1])
+    return u_curr    
+    
